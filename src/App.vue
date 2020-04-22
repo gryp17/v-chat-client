ce31e13acd96bbd43d36386992151e677162535d
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-04-22 15:54:21 +0300

    Added some initial styling

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,41 @@
 </template>
 
 <style lang="scss">
-	#app {
-		color: $color;
+	@font-face {
+		font-family: "Roboto Mono";
+		src: url(~@/assets/fonts/RobotoMono-Regular.woff2) format("woff2"),
+			url(~@/assets/fonts/RobotoMono-Regular.woff) format("woff"),
+			url(~@/assets/fonts/RobotoMono-Regular.ttf) format("truetype");
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	body, html {
+		margin: 0;
+		padding: 0;
+		height: 100%;
+	}
+
+	body {
+		font-family: "Roboto Mono";
+		color: $text-color;
+
+		:focus {
+			outline: none;
+		}
+
+		::-moz-focus-inner {
+			border: 0;
+		}
+
+		button:focus {
+			outline: none;
+		}
+
+		#app {
+			display: flex;
+			flex-direction: column;
+			height: 100%;
+		}
 	}
 </style>
