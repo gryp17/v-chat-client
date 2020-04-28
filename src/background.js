bd17c08d4623dd482d094d93e74ba3433be53fe8
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-04-28 18:16:09 +0300

    Added some styling to the initial setup page

--- src/background.js
+++ src/background.js
@@ -22,7 +22,7 @@ function createWindow() {
 		title: 'vChat',
 		frame: false,
 		minWidth: 900,
-		minHeigt: 600,
+		minHeight: 600,
 		webPreferences: {
 			nodeIntegration: true
 		}
--- src/views/InitialSetup.vue
+++ src/views/InitialSetup.vue
@@ -1,9 +1,8 @@
 <template>
 	<div class="initial-setup-page">
-		initial setup page
-		contains controls for setting up the workspace/server URL
-
 		<div class="form-wrapper">
+			<h4>Specify your vChat server URL</h4>
+
 			<FormInput
 				v-model="url"
 				type="text"
@@ -11,8 +10,6 @@
 				floating-label
 				placeholder="Server URL"
 			></FormInput>
-
-			<input v-model="url" placeholder="Server URL" type="text" />
 
 			<button @click="onSetServer">
 				Set server
@@ -51,9 +48,20 @@
 
 <style lang="scss">
 	.initial-setup-page {
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+
 		.form-wrapper {
 			margin: auto;
 			width: 70%;
+			padding: 10px;
+			background-color: $gray;
+
+			h4 {
+				margin-top: 10px;
+				text-align: center;
+			}
 		}
 	}
 </style>
